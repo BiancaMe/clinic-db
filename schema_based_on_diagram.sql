89773991418d5c5689541aebdab7b8f199985543
Bianca Lopez <bianquis0410@gmail.com>
2023-10-10 21:01:21 -0300

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,40 +1,52 @@
+CREATE DATABASE clinic;
+
 CREATE TABLE patients (
-id INT GENERATED ALWAYS AS IDENTITY, 
-full_name VARCHAR(20),
-age INT, 
-PRIMARY KEY(id)
+ id INT GENERATED ALWAYS AS IDENTITY, 
+ name VARCHAR(30),
+date_of_birth DATE,
+ PRIMARY KEY(id)
 );
+
 CREATE TABLE treatments(
-id INT GENERATED ALWAYS AS IDENTITY, 
-type VARCHAR,
-name VARCHAR,
-PRIMARY KEY(id)
+ id INT GENERATED ALWAYS AS IDENTITY, 
+ type VARCHAR,
+ name VARCHAR,
+ PRIMARY KEY(id)
 );
+
 CREATE TABLE invoices(
-id INT GENERATED ALWAYS AS IDENTITY, 
-total_amount DECIMAL,
-generated_at DATETIME,
-payed_at DATETIME,
-PRIMARY KEY(id)
+ id INT GENERATED ALWAYS AS IDENTITY, 
+ total_amount DECIMAL,
+ generated_at TIMESTAMP,
+ payed_at TIMESTAMP,
+ medical_history_id INT,
+ FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+ PRIMARY KEY(id)
 );
-CREATE TABLE invoice_item(
-id INT GENERATED ALWAYS AS IDENTITY, 
-unit_price DECIMAL,
-quantity INT,
-total_price DECIMAL,
-invoce_id INT,
-PRIMARY KEY(id)
+
+CREATE TABLE invoice_items(
+ id INT GENERATED ALWAYS AS IDENTITY, 
+ unit_price DECIMAL,
+ quantity INT,
+ total_price DECIMAL,
+ invoce_id INT,
+ treatment_id INT,
+ FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+ FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+ PRIMARY KEY(id)
 );
+
 CREATE TABLE medical_histories(
-id INT GENERATED ALWAYS AS IDENTITY, 
-admited_at DATETIME,
-patient_id INT,
-status VARCHAR,
-PRIMARY KEY(id)
+ id INT GENERATED ALWAYS AS IDENTITY, 
+ admited_at TIMESTAMP,
+ patient_id INT,
+ status VARCHAR,
+ FOREIGN KEY (patient_id) REFERENCES patients(id),
+ PRIMARY KEY(id)
 );
 
 CREATE TABLE designated_treatments (
- id INT GENERATED ALWAYS AS IDENTITY,
+  id INT GENERATED ALWAYS AS IDENTITY,
   medical_histories_id INT REFERENCES medical_histories(id),
   treatments_id INT REFERENCES treatments(id),
   PRIMARY KEY(id)
