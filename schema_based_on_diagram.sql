fcf115bd6b859c1613398368180b28a4f010f4e1
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-10-11 10:44:17 +0100

    Add context on schema.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -52,4 +52,8 @@ CREATE TABLE designated_treatments (
   PRIMARY KEY(id)
 );
 
-
+CREATE INDEX idx_mh_treat_medhist_id ON  designated_treatments (medical_history_id);
+CREATE INDEX idx_mh_treat_treat_id ON  designated_treatments (treatments_id);
+CREATE INDEX idx_inv_items_inv_id ON invoice_items (invoice_id);
+CREATE INDEX idx_inv_items_treat_id ON invoice_items (treatment_id);
+CREATE INDEX idx_invoices_medhist_id ON invoices (medical_history_id);
